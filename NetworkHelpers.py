e021ce6d0f0c7287fe3b1034c6bab20545fa75c1
Tomek <eskaeldev@gmail.com>
2021-04-02 10:52:56 +0200

    helper | params

--- NetworkHelpers.py
+++ NetworkHelpers.py
@@ -1,94 +1,96 @@
-def train_loop(criterion,
-               optimizer,
-               n_epochs,
-               no_improvement,
-               max_no_improve_epochs):
+import NetParams
+
+
+def train_loop(netparams: NetParams
+               no_improvement):
 
     valid_loss_min = np.Inf
     train_loss_array = []
     valid_loss_array = []
 
-    for epoch in range(1, n_epochs+1):
+    for epoch in range(1, netparams.n_epochs + 1):
 
         # early stopping
-    if no_improvement >= max_no_improve_epochs:
-        break
+        if no_improvement >= netparams.max_no_improve_epochs:
+            break
 
-    # keep track of training and validation loss
-    train_loss = 0.0
-    train_loss_tmp = 0.0
-    valid_loss = 0.0
+        # keep track of training and validation loss
+        train_loss = 0.0
+        train_loss_tmp = 0.0
+        valid_loss = 0.0
 
-    ###################
-    # train the model #
-    ###################
-    ######################
-    # evaluate the model #
-    ######################
+        ###################
+        # train the model #
+        ###################
+        train_model(netparams=netparams,
+                    train_loss_tmp=train_loss_tmp,
+                    train_loss=train_loss,
+                    epoch=epoch)
+        ######################
+        # evaluate the model #
+        ######################
+        evaluate_model(netparams=netparams
+                       train_loss=train_loss,
+                       valid_loss=valid_loss,
+                       valid_loss_min=valid_loss_min,
+                       valid_loss_array=valid_loss_array,
+                       train_loss_array=train_loss_array,
+                       epoch=epoch)
 
 
-def train_model(model,
-                train_loader,
-                optimizer,
-                criterion,
+def train_model(netparams: NetParams,
                 train_loss_tmp,
                 train_loss,
-                epoch,
-                n_epochs,
-                train_on_gpu):
-    model.train()
-    for batch_i, (data, target) in enumerate(train_loader):
+                epoch):
+    netparams.model.train()
+    for batch_i, (data, target) in enumerate(netparams.train_loader):
         # move tensors to GPU if CUDA is available
-        if train_on_gpu:
+        if netparams.train_on_gpu:
             data, target = data.cuda(), target.cuda()
         # clear the gradients of all optimized variables
-        optimizer.zero_grad()
+        netparams.optimizer.zero_grad()
         # forward pass: compute predicted outputs by passing inputs to the model
-        output = model(data)
+        netparams.output = netparams.model(data)
         # calculate the batch loss
-        loss = criterion(output, target)
+        loss = netparams.criterion(output, target)
         # backward pass: compute gradient of the loss with respect to model parameters
         loss.backward()
         # perform a single optimization step (parameter update)
-        optimizer.step()
+        netparams.optimizer.step()
         # update training loss
         train_loss_tmp += loss.item()
         train_loss += loss.item() * data.size(0)
 
         if batch_i % 20 == 19:    # print training loss every specified number of mini-batches
             print(
-                f'Epoch {epoch}, Batch {batch_i + 1}/{batch_size} loss: {(train_loss_tmp / 20):.16f}')
+                f'Epoch {epoch}, Batch {batch_i + 1}/{netparams.batch_size} loss: {(train_loss_tmp / 20):.16f}')
             train_loss_tmp = 0.0
 
 
-def evaluate_model(model,
-                   validation_loader,
+def evaluate_model(netparams: NetParams,
                    train_loss,
                    valid_loss
-                   criterion,
                    valid_loss_min,
                    valid_loss_array,
                    train_loss_array,
-                   epoch,
-                   n_epochs,
-                   train_on_gpu):
-    model.eval()
-    for batch_i, (data, target) in enumerate(validation_loader):
+                   epoch):
+    netparams.model.eval()
+    for batch_i, (data, target) in enumerate(netparams.validation_loader):
 
         # cuda
-        if train_on_gpu:
+        if netparams.train_on_gpu:
             data, target = data.cuda(), target.cuda()
 
-        y = model(data)
-        loss = criterion(y, target)
+        y = netparams.model(data)
+        loss = netparams.criterion(y, target)
         valid_loss += loss.item() * data.size(0)
 
-    train_loss = train_loss / len(train_loader.sampler)
-    valid_loss = valid_loss / len(validation_loader.sampler)
+    train_loss = train_loss / len(netparams.train_loader.sampler)
+    valid_loss = valid_loss / len(netparams.validation_loader.sampler)
 
     # print training/validation statistics
     print(
-        f'Evaluation\nEpoch: {epoch + 1}/{n_epochs+1} \tTraining Loss: {train_loss:.6f} \tValidation Loss: {valid_loss:.6f}')
+        f'Evaluation\nEpoch: {epoch + 1}/{netparams.n_epochs+1} \tTraining Loss: {train_loss:.6f} \tValidation Loss: {valid_loss:.6f}')
     train_loss_array.append(train_loss)
     valid_loss_array.append(valid_loss)
     # save model if validation loss has decreased
@@ -109,33 +111,29 @@ def plot_loss(loss_name: str, loss_array: set):
     plt.show()
 
 
-def test_model(classes,
-               test_loader,
-               train_on_gpu,
-               model,
-               criterion):
+def test_model(netparams: NetParams):
     # track test loss
     # over 5 flower classes
     test_loss = 0.0
-    class_correct = list(0. for i in range(len(classes)))
-    class_total = list(0. for i in range(len(classes)))
+    class_correct = list(0. for i in range(len(netparams.classes)))
+    class_total = list(0. for i in range(len(netparams.classes)))
 
-    model.eval()  # eval mode
+    netparams.model.eval()  # eval mode
     if train_on_gpu:
-        model.cuda()
+        netparams.model.cuda()
 
     # iterate over test data
     for data, target in test_loader:
 
-        if len(target.data) < batch_size:
+        if len(target.data) < netparams.batch_size:
             break
         # move tensors to GPU if CUDA is available
-        if train_on_gpu:
+        if netparams.train_on_gpu:
             data, target = data.cuda(), target.cuda()
         # forward pass: compute predicted outputs by passing inputs to the model
-        output = model(data)
+        output = netparams.model(data)
         # calculate the batch loss
-        loss = criterion(output, target)
+        loss = netparams.criterion(output, target)
         # update  test loss
         test_loss += loss.item() * data.size(0)
         # convert output probabilities to predicted class
@@ -145,7 +143,7 @@ def test_model(classes,
         correct = np.squeeze(correct_tensor.numpy()) if not train_on_gpu else np.squeeze(
             correct_tensor.cpu().numpy())
         # calculate test accuracy for each object class
-        for i in range(batch_size):
+        for i in range(netparams.batch_size):
             label = target.data[i]
             class_correct[label] += correct[i].item()
             class_total[label] += 1
---
+++ netparams.py
@@ -0,0 +1,22 @@
+class NetParams:
+    def __init__(self,
+                 model,
+                 criterion,
+                 optimizer,
+                 n_epochs,
+                 max_no_improve_epochs,
+                 train_on_gpu,
+                 train_loader,
+                 test_loader,
+                 validation_loader,
+                 batch_size):
+        self.model
+        self.criterion
+        self.optimizer
+        self.n_epochs
+        self.max_no_improve_epochs
+        self.train_on_gpu
+        self.train_loader = train_loader
+        self.test_loader = test_loader
+        self.validation_loader = validation_loader
+        self.batch_size = batch_size
