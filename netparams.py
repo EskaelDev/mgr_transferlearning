43147ce28dd3cef615f33c6cfcd6b1d63f32d5f9
Tomek <eskaeldev@gmail.com>
2021-04-10 14:09:58 +0200

    fixes

--- netparams.py
+++ netparams.py
@@ -10,13 +10,13 @@ class NetParams:
                  test_loader,
                  validation_loader,
                  batch_size):
-        self.model
-        self.criterion
-        self.optimizer
-        self.n_epochs
-        self.max_no_improve_epochs
-        self.train_on_gpu
-        self.train_loader = train_loader
-        self.test_loader = test_loader
-        self.validation_loader = validation_loader
-        self.batch_size = batch_size
+        self.model = model
+        self.criterion = criterion
+        self.optimizer = optimizer
+        self.n_epochs = n_epochs
+        self.max_no_improve_epochs = max_no_improve_epochs
+        self.train_on_gpu = train_on_gpu
+        self.train_loader = train_loader = train_loader
+        self.test_loader = test_loader = test_loader
+        self.validation_loader = validation_loader = validation_loader
+        self.batch_size = batch_size = batch_size
