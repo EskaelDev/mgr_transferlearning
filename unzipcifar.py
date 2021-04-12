8dd5abea664f6b2267992f0952f2c48277d2de50
Tomek <eskaeldev@gmail.com>
2021-04-12 17:46:33 +0200

    fix

--- unzipcifar.py
+++ unzipcifar.py
@@ -4,7 +4,7 @@ import zipfile
 
 def unzip_cifar():
     path_to_zip_file = os.path.join(
-        'drive/MyDrive/cifar/', "state_dicts.zip")
+        'drive/MyDrive/dataset/', "state_dicts.zip")
     directory_to_extract_to = os.path.join(os.getcwd(), "cifar10_models")
     with zipfile.ZipFile(path_to_zip_file, "r") as zip_ref:
         zip_ref.extractall(directory_to_extract_to)
