6d6f1ada18e81d17fff00d3e7db28aa4caad2d73
Tomek <eskaeldev@gmail.com>
2021-04-30 17:34:51 +0200

    top k

--- NetworkHelpers.py
+++ NetworkHelpers.py
@@ -180,6 +180,7 @@ def test_model(netparams: NetParams, working_ds: DatasetModel):
     test_loss = 0.0
     class_correct = list(0. for i in range(working_ds.class_num))
     class_total = list(0. for i in range(working_ds.class_num))
+    top_k = []
 
     netparams.model.eval()  # eval mode
     if netparams.train_on_gpu:
@@ -211,6 +212,8 @@ def test_model(netparams: NetParams, working_ds: DatasetModel):
             class_correct[label] += correct[i].item()
             class_total[label] += 1
 
+        top_k.append(top_k_1_5(output, target))
+
     # calculate avg test loss
     test_loss = test_loss / len(netparams.test_loader.dataset)
     print('Test Loss: {:.6f}\n'.format(test_loss))
@@ -227,7 +230,8 @@ def test_model(netparams: NetParams, working_ds: DatasetModel):
     print('\nTest Accuracy (Overall): %2d%% (%2d/%2d)' % (
         100. * np.sum(class_correct) / np.sum(class_total),
         np.sum(class_correct), np.sum(class_total)))
-    return 100. * np.sum(class_correct) / np.sum(class_total)
+    mean_1, mean_5 = mean_top_k(top_k)
+    return 100. * np.sum(class_correct) / np.sum(class_total), mean_1, mean_5
 
 
 def plot_test_results(netparams: NetParams, working_ds: DatasetModel):
@@ -251,3 +255,80 @@ def plot_test_results(netparams: NetParams, working_ds: DatasetModel):
         plt.imshow(np.transpose(images[idx], (1, 2, 0)))
         ax.set_title("{}\n({})".format(working_ds.classes[preds[idx]], working_ds.classes[labels[idx]]),
                      color=("green" if preds[idx] == labels[idx].item() else "red"))
+
+
+def top_k_1_5(output: torch.Tensor, target: torch.Tensor, topk=(1, 5)) -> List[torch.FloatTensor]:
+    """
+    Computes the accuracy over the k top predictions for the specified values of k
+    In top-5 accuracy you give yourself credit for having the right answer
+    if the right answer appears in your top five guesses.
+
+    ref:
+    - https://pytorch.org/docs/stable/generated/torch.topk.html
+    - https://discuss.pytorch.org/t/imagenet-example-accuracy-calculation/7840
+    - https://gist.github.com/weiaicunzai/2a5ae6eac6712c70bde0630f3e76b77b
+    - https://discuss.pytorch.org/t/top-k-error-calculation/48815/2
+    - https://stackoverflow.com/questions/59474987/how-to-get-top-k-accuracy-in-semantic-segmentation-using-pytorch
+
+    :param output: output is the prediction of the model e.g. scores, logits, raw y_pred before normalization or getting classes
+    :param target: target is the truth
+    :param topk: tuple of topk's to compute e.g. (1, 2, 5) computes top 1, top 2 and top 5.
+    e.g. in top 2 it means you get a +1 if your models's top 2 predictions are in the right label.
+    So if your model predicts cat, dog (0, 1) and the true label was bird (3) you get zero
+    but if it were either cat or dog you'd accumulate +1 for that example.
+    :return: list of topk accuracy [top1st, top2nd, ...] depending on your topk input
+    """
+    with torch.no_grad():
+        # ---- get the topk most likely labels according to your model
+        # get the largest k \in [n_classes] (i.e. the number of most likely probabilities we will use)
+        # max number labels we will consider in the right choices for out model
+        maxk = max(topk)
+        batch_size = target.size(0)
+
+        # get top maxk indicies that correspond to the most likely probability scores
+        # (note _ means we don't care about the actual top maxk scores just their corresponding indicies/labels)
+        # _, [B, n_classes] -> [B, maxk]
+        _, y_pred = output.topk(k=maxk, dim=1)
+        # [B, maxk] -> [maxk, B] Expects input to be <= 2-D tensor and transposes dimensions 0 and 1.
+        y_pred = y_pred.t()
+
+        # - get the credit for each example if the models predictions is in maxk values (main crux of code)
+        # for any example, the model will get credit if it's prediction matches the ground truth
+        # for each example we compare if the model's best prediction matches the truth. If yes we get an entry of 1.
+        # if the k'th top answer of the model matches the truth we get 1.
+        # Note: this for any example in batch we can only ever get 1 match (so we never overestimate accuracy <1)
+        # [B] -> [B, 1] -> [maxk, B]
+        target_reshaped = target.view(1, -1).expand_as(y_pred)
+        # compare every topk's model prediction with the ground truth & give credit if any matches the ground truth
+        # [maxk, B] were for each example we know which topk prediction matched truth
+        correct = (y_pred == target_reshaped)
+        # original: correct = pred.eq(target.view(1, -1).expand_as(pred))
+
+        # -- get topk accuracy
+        list_topk_accs = []  # idx is topk1, topk2, ... etc
+        for k in topk:
+            # get tensor of which topk answer was right
+            ind_which_topk_matched_truth = correct[:k]  # [maxk, B] -> [k, B]
+            # flatten it to help compute if we got it correct for each example in batch
+            # [k, B] -> [kB]
+            flattened_indicator_which_topk_matched_truth = ind_which_topk_matched_truth.reshape(
+                -1).float()
+            # get if we got it right for any of our top k prediction for each example in batch
+            tot_correct_topk = flattened_indicator_which_topk_matched_truth.float().sum(
+                dim=0, keepdim=True)  # [kB] -> [1]
+            # compute topk accuracy - the accuracy of the mode's ability to get it right within it's top k guesses/preds
+            topk_acc = tot_correct_topk / batch_size  # topk accuracy for entire batch
+            list_topk_accs.append(topk_acc)
+        # list of topk accuracies for entire batch [topk1, topk2, ... etc]
+        return list_topk_accs
+
+
+def mean_top_k(top_k: []) -> tuple:
+    sum_1 = 0
+    sum_5 = 0
+
+    for i in top_k:
+        sum_1 += i[0]
+        sum_5 += i[1]
+
+    return sum_1 / len(top_k), sum_5 / len(top_k)
--- trainstats.py
+++ trainstats.py
@@ -6,6 +6,8 @@ import json
 
 class TrainStats:
     accuracy = 0
+    k1 = 0
+    k5 = 0
 
     def __init__(self, model_name, train_loss_array, valid_loss_array, train_accuracy_array, valid_accuracy_array, best_epoch, total_time, train_time_sum, eval_time_sum):
         self.model_name = model_name
